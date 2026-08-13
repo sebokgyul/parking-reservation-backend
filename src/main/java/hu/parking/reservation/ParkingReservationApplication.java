418eab3978448eb5873a55d8e7a353bf7a16cc38
Gyula Sebok <gyula.sbk@gmail.com>
2026-08-13 23:03:33 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/hu/parking/reservation/ParkingReservationApplication.java
@@ -0,0 +1,12 @@
+package hu.parking.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingReservationApplication.class, args);
+    }
+}
